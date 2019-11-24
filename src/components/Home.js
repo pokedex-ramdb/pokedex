35737e6af17e1dd35c266c06fc87f3ad018f0574
rizkianakbar <rizkianakbar4@gmail.com>
2019-11-25 01:25:16 +0700

    add list-stack

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,18 @@ import React from "react";
 import "../App.css";
 import { Jumbotron, Container, Row, Col, Card, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import yarn from "./yarn.png";
+import js from "./js.png";
+import mtui from "./mtui.png";
+import nodejs from "./nodejs.png";
+import react from "./react.png";
+import vc from "./vc.png";
+import rb from "./rb.png";
+import css from "./css.svg";
+import mozilla from "./mozilla.png";
+import axios from "./axios.png";
+import reacticons from "./reacticons.png";
+import ubuntu from "./ubuntu.png";
 
 const Home = () => {
   return (
@@ -98,6 +110,86 @@ const Home = () => {
           </Button>
         </div>
       </div>
+      <Container>
+        <Row>
+          <Col xs={12} style={{ textAlign: "center" }}>
+            <h1 className="navnamee" style={{ marginTop: "50px" }}>
+              List Stack
+            </h1>
+            <hr
+              style={{
+                width: "300px",
+                border: "1px solid black",
+                borderRadius: "50%"
+              }}
+            />
+            <p className="ket">Find your favorite pokemon</p>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col xs={4} lg={2} style={{ textAlign: "center", marginTop: "20px" }}>
+            <img src={js} alt="Yarn" className="liststack"></img>
+          </Col>
+          <Col xs={4} lg={2} style={{ textAlign: "center", marginTop: "20px" }}>
+            <img src={react} alt="Yarn" className="liststack"></img>
+          </Col>
+          <Col xs={4} lg={2} style={{ textAlign: "center", marginTop: "20px" }}>
+            <img
+              src={nodejs}
+              style={{ width: "130px" }}
+              alt="Yarn"
+              className="liststack"
+            ></img>
+          </Col>
+
+          <Col xs={4} lg={2} style={{ textAlign: "center", marginTop: "20px" }}>
+            <img
+              src={yarn}
+              alt="Yarn"
+              style={{ width: "130px" }}
+              className="liststack"
+            ></img>
+          </Col>
+          <Col xs={4} lg={2} style={{ textAlign: "center", marginTop: "20px" }}>
+            <img src={mtui} alt="Yarn" className="liststack"></img>
+          </Col>
+          <Col xs={4} lg={2} style={{ textAlign: "center", marginTop: "20px" }}>
+            <img src={vc} alt="Yarn" className="liststack"></img>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={4} lg={2} style={{ textAlign: "center", marginTop: "20px" }}>
+            <img src={ubuntu} alt="Yarn" className="liststack"></img>
+          </Col>
+          <Col xs={4} lg={2} style={{ textAlign: "center", marginTop: "20px" }}>
+            <img src={reacticons} alt="Yarn" className="liststack"></img>
+          </Col>
+          <Col xs={4} lg={2} style={{ textAlign: "center", marginTop: "20px" }}>
+            <img
+              src={css}
+              style={{ width: "50px" }}
+              alt="Yarn"
+              className="liststack"
+            ></img>
+          </Col>
+
+          <Col xs={4} lg={2} style={{ textAlign: "center", marginTop: "20px" }}>
+            <img
+              src={axios}
+              alt="Yarn"
+              style={{ width: "130px" }}
+              className="liststack"
+            ></img>
+          </Col>
+          <Col xs={4} lg={2} style={{ textAlign: "center", marginTop: "20px" }}>
+            <img src={rb} alt="Yarn" className="liststack"></img>
+          </Col>
+          <Col xs={4} lg={2} style={{ textAlign: "center", marginTop: "20px" }}>
+            <img src={mozilla} alt="Yarn" className="liststack"></img>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
